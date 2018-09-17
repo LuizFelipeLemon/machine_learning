53b213e47a42745702ed3b89a7d6dde4130e9aed
luizfelipelemon <luizfelipeplm@gmail.com>
2018-09-17 18:56:18 -0300

    final updates

--- mlp-arduino/NeuralNetwork.cpp
+++
@@ -1,7 +0,0 @@
-
-NeuralNetwork::NeuralNetwork() {
-  cout << "Luiz";
-  cout << "Luiz";
-}
-
-NeuralNetwork::~NeuralNetwor() {}
---
+++ mlp-arduino/arduino_code/arduino_code.ino
@@ -0,0 +1,12 @@
+#include "NeuralNetwork.h"
+NeuralNetwork brain;
+
+void setup() {
+  Serial.begin(9600);
+  brain.load();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
