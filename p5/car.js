f1e828d43d1787b798ae1fea501f9092b2299c61
VictorPetch <32557771+VictorPetch@users.noreply.github.com>
2018-10-31 20:45:54 -0300

    Add files via upload

--- p5/car.js
+++ p5/car.js
@@ -1,1 +1,16 @@
-function Car(){}
+function Car(x,y,Mov){
+	this.Pos_X = x;
+	this.Pos_Y = y;
+	this.Mov_number = Mov;
+	
+}
+
+Car.prototype.show = function(){
+	fill(255,0,0);    
+	stroke(0);
+	console.log(this.Pos_Y);
+    rect(40*this.Pos_X,40* this.Pos_Y, 19,1 9);
+
+
+
+}
--- p5/main.js
+++ p5/main.js
@@ -1,15 +1,17 @@
 
-var b;
+var b,c;
 
 function setup() {
     createCanvas(1280, 640);
     b = new Board(1280, 640, 20);
+    c = new Car(5,5,34);
 
 }
 
 function draw() {
 
-    b.draw()
+   // b.draw()
+    c.show()
 }
 
 function mousePressed() {
